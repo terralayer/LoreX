ca209b16e9b3978baeb30e96412e5929e1b101c0
TerraLayer <321948909+terralayer@users.noreply.github.com>
2026-09-04 08:18:50 -0700

    feat: persist encrypted NNTP provider configuration

---
+++ backend/lorex/nntp/__init__.py
@@ -0,0 +1,9 @@
+from lorex.nntp.models import NntpProvider, NntpProviderGroup, ProviderSecretUpdate
+from lorex.nntp.repository import PostgresNntpProviderRepository
+
+__all__ = [
+    "NntpProvider",
+    "NntpProviderGroup",
+    "ProviderSecretUpdate",
+    "PostgresNntpProviderRepository",
+]
